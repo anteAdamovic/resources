cb8700d75a877572f8d5780b151adf29aa1bba75
anteAdamovic <ante.adamovic93@gmail.com>
2020-07-04 15:17:24 +0200

    Rename S3UploadsService and add missing index to services

---
+++ node/ts/utility/services/index.ts
@@ -0,0 +1,6 @@
+export * from './email.service';
+export * from './logger.service';
+export * from './payments.service';
+export * from './s3-upload.service';
+export * from './slack.service';
+export * from './validation.service';
